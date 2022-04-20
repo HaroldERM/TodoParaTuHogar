0617bc4769162930b51f553b6f5802de7d18a7c7
HaroldERM <enrique5129876@estudiantec.cr>
2022-04-20 01:35:09 -0600

    4 files modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-import { MatTabsModule } from '@angular/material/tabs';
-
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login.component';
 import { TabsComponent } from './tabs.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, MatTabsModule],
-  exports: [MatTabsModule],
+  imports: [BrowserModule, FormsModule],
   declarations: [AppComponent, LoginComponent, TabsComponent],
   bootstrap: [AppComponent]
 })
