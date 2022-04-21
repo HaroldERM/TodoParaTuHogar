0c586417b070bf8981d8f8d9459f2194053457e7
HaroldERM <enrique5129876@estudiantec.cr>
2022-04-20 20:25:42 -0600

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login.component';
@@ -8,9 +9,10 @@ import { TabsComponent } from './tabs.component';
 import { AboutUsComponent } from './about-us.component';
 import { ProductsComponent } from './products.component';
 import { AddProductComponent } from './add-product.component';
+import { CartComponent } from './cart.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   declarations: [
     AppComponent,
     LoginComponent,
@@ -18,6 +20,7 @@ import { AddProductComponent } from './add-product.component';
     AboutUsComponent,
     ProductsComponent,
     AddProductComponent,
+    CartComponent,
   ],
   bootstrap: [AppComponent],
 })
