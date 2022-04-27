1b7bd3587b08d917af0a81c33a7a5501b651891b
HaroldERM <enrique5129876@estudiantec.cr>
2022-04-26 22:25:39 -0600

    3 files modified, 3 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { FirebaseService } from './firebase.service';
-import { UserInfo } from './login.component';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app',
@@ -8,24 +6,5 @@ import { UserInfo } from './login.component';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  email: string;
-  password: string;
-
-  constructor(public firebaseService: FirebaseService) {}
-
-  register() {
-    this.firebaseService.register(this.email, this.password);
-    this.email = '';
-    this.password = '';
-  }
-
-  login() {
-    this.firebaseService.login(this.email, this.password);
-    this.email = '';
-    this.password = '';
-  }
-
-  logout() {
-    this.firebaseService.logout();
-  }
+  name = 'Todo para tu hogar';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { AngularFireModule } from '@angular/fire';
 import { AppRoutingModule } from './app-routing.module';
-import { FirebaseService } from './firebase.service';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login.component';
-import { LoginComponent2 } from './login-transition.component';
 import { TabsComponent } from './tabs.component';
 import { AboutUsComponent } from './about-us.component';
 import { ProductsComponent } from './products.component';
@@ -15,24 +12,10 @@ import { AddProductComponent } from './add-product.component';
 import { CartComponent } from './cart.component';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule,
-    AppRoutingModule,
-    AngularFireModule.initializeApp({
-      apiKey: 'AIzaSyCI3-YRFBWH8MCq57m6nPiC4MlQL41SEnM',
-      authDomain: 'todoparatuhogar-ed9a6.firebaseapp.com',
-      projectId: 'todoparatuhogar-ed9a6',
-      storageBucket: 'todoparatuhogar-ed9a6.appspot.com',
-      messagingSenderId: '616941008723',
-      appId: '1:616941008723:web:dfeb6c124aacf1a365d3a6',
-    }),
-  ],
-  providers: [FirebaseService],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   declarations: [
     AppComponent,
     LoginComponent,
-    LoginComponent2,
     TabsComponent,
     AboutUsComponent,
     ProductsComponent,
