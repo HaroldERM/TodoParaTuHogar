e320b3d34bf8e666ee30f3e19a70c8c6694281d9
HaroldERM <enrique5129876@estudiantec.cr>
2022-04-26 18:24:15 -0600

    4 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { AngularFireModule} from '@angular/fire'
 import { AppRoutingModule } from './app-routing.module';
+import { FirebaseService} from './firebase.service'
+
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login.component';
@@ -12,7 +15,21 @@ import { AddProductComponent } from './add-product.component';
 import { CartComponent } from './cart.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    FormsModule, 
+    AppRoutingModule,
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyCI3-YRFBWH8MCq57m6nPiC4MlQL41SEnM",
+      authDomain: "todoparatuhogar-ed9a6.firebaseapp.com",
+      projectId: "todoparatuhogar-ed9a6",
+      storageBucket: "todoparatuhogar-ed9a6.appspot.com",
+      messagingSenderId: "616941008723",
+      appId: "1:616941008723:web:dfeb6c124aacf1a365d3a6")
+  ],
+  providers: [
+    FirebaseService
+  ],
   declarations: [
     AppComponent,
     LoginComponent,
